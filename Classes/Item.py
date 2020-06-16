3c770ca477daada03d64ff4d48f28dbc6b5b9c80
nadir meddour <amazconsult@gmail.com>
2020-06-16 12:01:47 +0200

    Firstsave

---
+++ Classes/Item.py
@@ -0,0 +1,19 @@
+
+#CREATE human class
+class human:
+    
+    def __init__(self, fname):
+        print("création d'un humain")   
+        self.firstname = fname
+print("lancement du programme")
+
+#stock intoo a variable to create objet agent1 et agent2  
+agent1 = human()
+print("firsname of agent1 -> {}" .format())
+
+agent2 = human()
+print("prénom agent2 -> {}" .format())
+
+
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+#coding: UTF-8
+
+from Classes.Item import Item
+MacGiver = Item(10,10)
+MacGiver.debug()
